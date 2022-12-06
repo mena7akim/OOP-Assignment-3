3b34a8c5719959e4fae2e9fcea23083fd5ad74e0
mena7akim <mena7akim@gmail.com>
2022-12-06 16:12:27 +0200

    Created Assignment Files

---
+++ Task 1/XYVector.h
@@ -0,0 +1,11 @@
+#ifndef TASK_1_XYVECTOR_H
+#define TASK_1_XYVECTOR_H
+
+#include "iterator.h"
+
+class XYVector {
+
+};
+
+
+#endif
---
+++ Task 1/iterator.h
@@ -0,0 +1,12 @@
+#ifndef TASK_1_ITERATOR_H
+#define TASK_1_ITERATOR_H
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class iterator {
+
+};
+
+
+#endif
